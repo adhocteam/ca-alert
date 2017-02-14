c6ad1a958f7b8b5fdcf260b46266a92dc4c347ee
Aubrey Holland <aubreyholland@gmail.com>
2017-02-14 13:50:04 -0500

    Get the rubocops passing (#17)

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -18,7 +18,6 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
@@ -39,7 +38,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
