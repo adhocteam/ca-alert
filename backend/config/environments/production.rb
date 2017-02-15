63b449bc1b36e270214326ca7d7673ed1bd9f146
Aubrey Holland <aubreyholland@gmail.com>
2017-02-15 17:45:30 -0500

    Fix a syntax error

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -75,5 +75,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'ca-alert.herokuapp.com', protocol: 'https'
+  config.action_mailer.default_url_options = { host: 'ca-alert.herokuapp.com', protocol: 'https' }
 end
