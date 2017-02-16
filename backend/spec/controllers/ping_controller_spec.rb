edca32cfc4177c5705ed961c705c05acacb8d897
Aubrey Holland <aubreyholland@gmail.com>
2017-02-16 08:47:08 -0500

    Add a spec for swagger controller

--- backend/spec/controllers/ping_controller_spec.rb
+++ backend/spec/controllers/ping_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PingController, type: :request do
-  it 'should create a user' do
+  it 'should return success' do
     get '/ping'
     expect(response).to be_success
     expect(response.body).to eq('success')
