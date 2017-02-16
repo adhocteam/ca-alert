c60e51c36988ffc481039dea78d9808da8528369
Aubrey Holland <aubreyholland@gmail.com>
2017-02-16 11:46:09 -0500

    Add api-based authentication (#29)
    * Add devise_token_auth for authentication
    
    This adds devise_token_auth, which handles REST-based authentication for
    APIs. It pretty much does all of the auth magic for us, so after
    configuring it I just needed to add some specs to show it working.
    
    * Fix a syntax error
    
    * remove coverage files from repo

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -22,6 +22,10 @@ module CaAlert
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.after_initialize do
+      Rails.application.routes.default_url_options = config.action_mailer.default_url_options
+    end
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
+
   get '/ping', to: "ping#show", as: :ping
 
   resources :apidocs, only: [:index]
