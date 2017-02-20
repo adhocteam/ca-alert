2e4204dde255f35ac1c5172b35e3ef2eed510211
Paul Smith <paulsmith@pobox.com>
2017-02-20 11:09:52 -0600

    Add placeholder home page

---
+++ web/src/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default React.createClass({
+  render() {
+    return <h1>App home/dashboard goes here</h1>;
+  }
+});
--- web/src/index.jsx
+++ web/src/index.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, hashHistory } from "react-router";
 import SignUpForm from "./SignUpForm";
+import Home from "./Home";
 
 ReactDOM.render(
   <Router history={hashHistory}>
-     <Route path="/account/signup" component={SignUpForm} />
+    <Route path="/" component={Home} />
+    <Route path="/account/signup" component={SignUpForm} />
   </Router>,
   document.getElementById("app")
 );
