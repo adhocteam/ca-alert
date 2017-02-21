11a475f7daa8a69236602ce485ab6d1a39ac2ca9
Aubrey Holland <aubreyholland@gmail.com>
2017-02-21 16:09:06 -0500

    fix schema date

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220103738) do
+ActiveRecord::Schema.define(version: 20170220214814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
