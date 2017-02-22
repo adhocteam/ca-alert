0b5a0b00b2738b5f1003ab4bbc121a3738feb986
Aubrey Holland <aubreyholland@gmail.com>
2017-02-22 12:58:25 -0500

    Perform hazard intersections (#71)
    * Perform hazard intersections
    
    When a new hazard is created, this performs intersections between the
    hazrd and all of the places in the database. It then creates alerts for
    each of the hazards, which themselves cause email and SMS messages to be
    delivered to users.
    
    * Refer to coordinates more correctly

---
+++ backend/app/models/concerns/has_coordinates.rb
@@ -0,0 +1,35 @@
+module HasCoordinates
+  extend ActiveSupport::Concern
+
+  included do
+    attr_writer :latitude
+    attr_writer :longitude
+
+    before_save :convert_coordinates_to_geo
+    validate :ensure_coordinates_present
+  end
+
+  def as_json(options = {})
+    h = super(except: [:coord])
+    h['longitude'] = coord.lon
+    h['latitude'] = coord.lat
+    h
+  end
+
+  private
+
+  def ensure_coordinates_present
+    if @latitude.blank? && coord&.lat.blank?
+      errors.add(:latitude, 'can\'t be blank')
+    end
+    if @longitude.blank? && coord&.lon.blank?
+      errors.add(:longitude, 'can\'t be blank')
+    end
+  end
+
+  def convert_coordinates_to_geo
+    if @latitude && @longitude
+      self.coord = "POINT (#{@longitude} #{@latitude})"
+    end
+  end
+end
