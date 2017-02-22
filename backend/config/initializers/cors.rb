ca62ddfcd7d74813ffe57d5cc712d280ab1a164b
Paul Smith <paulsmith@pobox.com>
2017-02-22 08:28:54 -0600

    Remove dupe header

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -16,7 +16,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       # We need to explicitly expose non-standard CORS response headers via
       # Access-Control-Expose-Headers in order for client code to get access to
       # them, these are from devise token auth
-      expose: ['access-token', 'client', 'expiry', 'token-type', 'uid', 'client']
+      expose: ['access-token', 'client', 'expiry', 'token-type', 'uid']
     )
   end
 end
