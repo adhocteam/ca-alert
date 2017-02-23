fccc9b6dda5385c9027fdad2de2ff70c4c27e347
Aubrey Holland <aubreyholland@gmail.com>
2017-02-23 10:25:30 -0500

    Add an endpoint for the user to get all of their alerts (#73)

---
+++ backend/lib/alerts_controller_swagger_blocks.rb
@@ -0,0 +1,67 @@
+class AlertsControllerSwaggerBlocks
+  include Swagger::Blocks
+
+  swagger_path '/alerts' do
+    operation :get do
+      key :description, 'Retrieve your list of alerts'
+      key :operationId, 'listAlerts'
+      key :produces, ['application/json']
+      parameter do
+        key :name, :uid
+        key :in, :header
+        key :description, 'UID of the user'
+        key :required, true
+        schema do
+          key :type, :string
+        end
+      end
+      parameter do
+        key :name, :access_token
+        key :in, :header
+        key :description, 'Access token for the user'
+        key :required, true
+        schema do
+          key :type, :string
+        end
+      end
+      parameter do
+        key :name, :client
+        key :in, :header
+        key :description, 'Client value for the user'
+        key :required, true
+        schema do
+          key :type, :string
+        end
+      end
+
+      response 200 do
+        key :description, 'alert list response'
+        schema do
+          key :'$ref', :AlertsResponse
+        end
+      end
+
+      response 401 do
+        key :description, 'invalid authentication response'
+        schema do
+          key :'$ref', :Response
+        end
+      end
+    end
+  end
+
+  swagger_schema :AlertsResponse do
+    property :status do
+      key :type, :string
+    end
+    property :data do
+      key :type, :array
+      items do
+        key :'$ref', :Alert
+      end
+    end
+    property :errors do
+      key :'$ref', :Errors
+    end
+  end
+end
