f449b65bcb34792f0d7408dfdf5165ba6a1f22e0
Paul Smith <paulsmith@pobox.com>
2017-03-01 08:40:01 -0600

    Fix tests

--- web/src/Nav.jsx
+++ web/src/Nav.jsx
@@ -5,15 +5,15 @@ export default class Nav extends React.Component {
   render() {
     if (!isLoggedIn()) {
       return (
-
-            <a className="usa-button usa-button-outline" href="#/account/signin">Sign in</a>
-
+        <a className="usa-button usa-button-outline" href="#/account/signin">
+          Sign in
+        </a>
       );
     } else {
       let user = loggedInUser();
       return (
         <ul className="usa-unstyled-list usa-nav-secondary-links">
-        
+
           <li>
             <a href="#/dashboard">{user.name ? user.name : user.email}</a>
           </li>
