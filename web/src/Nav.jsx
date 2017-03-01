cc9fbda8eadda1fb51853776f0b60b12f86d4761
Danny Chapman <danny@ducstudio.com>
2017-03-01 09:42:52 -0500

    WIP: Initial frontend design (#126)
    * Stubbed out initial front end design
    
    Design updates for:
    - Homepage
    - Header/footer
    - Nav
    
    * Basic styles for header, footer, splash page
    
    - Added additional HTMl tags for index elements
    - Added footer images
    - Added background image
    - Created basic styles for CA site implementation

--- web/src/Nav.jsx
+++ web/src/Nav.jsx
@@ -5,29 +5,15 @@ export default class Nav extends React.Component {
   render() {
     if (!isLoggedIn()) {
       return (
-        <ul className="usa-unstyled-list usa-nav-secondary-links">
-          <li className="js-search-button-container">
-            <button className="usa-header-search-button js-search-button">
-              Search
-            </button>
-          </li>
-          <li>
-            <a href="#/account/signup">Sign up for new account</a>
-          </li>
-          <li>
-            <a href="#/account/signin">Sign in to your account</a>
-          </li>
-        </ul>
+        <a className="usa-button usa-button-outline" href="#/account/signin">
+          Sign in
+        </a>
       );
     } else {
       let user = loggedInUser();
       return (
         <ul className="usa-unstyled-list usa-nav-secondary-links">
-          <li className="js-search-button-container">
-            <button className="usa-header-search-button js-search-button">
-              Search
-            </button>
-          </li>
+
           <li>
             <a href="#/dashboard">{user.name ? user.name : user.email}</a>
           </li>
