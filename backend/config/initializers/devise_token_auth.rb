e476e9d76fde8e7d3d42febb05039ffd250e823e
Aubrey Holland <aubreyholland@gmail.com>
2017-03-01 21:58:54 -0500

    rubocoppin

--- backend/config/initializers/devise_token_auth.rb
+++ backend/config/initializers/devise_token_auth.rb
@@ -1,5 +1,4 @@
 DeviseTokenAuth.setup do |config|
-  url_options = Rails.application.config.action_mailer.default_url_options
   if Rails.env.production?
     config.default_confirm_success_url = 'https://ca-alert-prototype.s3.amazonaws.com/index.html#/account/signin'
   else
