99610884fe617787a92c4a8d89b9762cdbda1b44
Graham Smith <graham@adhocteam.us>
2017-03-03 10:10:44 -0500

    Change sign in link to sign up
    * When the user isn't logged in
    * Closes #265

--- web/test/nav_spec.js
+++ web/test/nav_spec.js
@@ -10,9 +10,9 @@ describe("Nav Component", () => {
   it("Should show a non-logged in version by default", () => {
     const el = shallow(<Nav />);
 
-    const signin = el.find({ href: "#/account/signin" });
+    const signin = el.find('a');
     expect(signin.length).to.equal(1);
-    expect(signin.text()).to.contain("Sign in");
+    expect(signin.text()).to.contain("Sign up");
   });
 
   it("Should show a logged in version w/ a valid session", () => {
@@ -32,19 +32,19 @@ describe("Nav Component", () => {
     const el = mount(<Nav />);
 
     // Signed out by default
-    expect(el.find({href: "#/account/signin" })).to.have.length(1);
+    expect(el.find({href: "#/" })).to.have.length(1);
     expect(el.find({href: "#/account/signout" })).to.have.length(0);
 
     newLoginSession('x', 'y', 'z', 'a', 'b');
 
     // Should now be signed in
-    expect(el.find({href: "#/account/signin" })).to.have.length(0);
+    expect(el.find({href: "#/" })).to.have.length(0);
     expect(el.find({href: "#/account/signout" })).to.have.length(1);
 
     logout();
 
     // and signed back out
-    expect(el.find({href: "#/account/signin" })).to.have.length(1);
+    expect(el.find({href: "#/" })).to.have.length(1);
     expect(el.find({href: "#/account/signout" })).to.have.length(0);
   });
 });
