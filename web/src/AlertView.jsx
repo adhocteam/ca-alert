65ba072aaaddffb3926755e4f6f1e7594b395b5d
dannychapman <danny@ducstudio.com>
2017-03-03 00:41:19 -0500

    Fixed broken specs

--- web/src/AlertView.jsx
+++ web/src/AlertView.jsx
@@ -19,17 +19,25 @@ export default function AlertView(props) {
 
     return (
       <section>
-      <div className="form-section">
+        <div className="form-section">
         <h2>{(props.hazard.is_emergency) ? 'EMERGENCY: ' : ''}{props.hazard.title}</h2>
+
           <span className="usa-label-big">{props.hazard.category}</span>
+
           <span className="start-date">{start.format("lll")}</span>
-          <hr />
+
           <p>{props.hazard.message}</p>
+
+        <div className="more-details">
           <a href={`tel:${props.hazard.phone_number}`}>{props.hazard.phone_number}</a>
-          {linkPart}
-          <div className="map-section">
-            <Map lat={props.hazard.latitude} lng={props.hazard.longitude} />
-          </div>  
+        </div>
+
+        {linkPart}
+
+        <div className="map-section">
+        <h3>Location</h3>
+        <Map lat={props.hazard.latitude} lng={props.hazard.longitude} />
+        </div>
         </div>
       </section>
     );
